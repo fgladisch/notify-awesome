de2b26a0920d6e80813a46165988b85917ea0c01
Felix Gladisch <felix@felixgladisch.de>
2014-12-05 19:30:59 +0100

    No longer polluting the global namespace. Different public functions for generating notifications.

--- notify.js
+++ notify.js
@@ -15,63 +15,75 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-var notificationUpdateInterval = 200;
-var notificationId = 0;
-var notificationResource = {};
+/*jslint browser: true, vars: true, forin: true*/
+/*globals window, console, $*/
 
-$(document).ready( function() {
-  setInterval(updateNotifications, notificationUpdateInterval);
-});
-
-function Notification(element, duration) {
-  this.initTime = (new Date).getTime();
-  this.element = element;
-  this.duration = duration * 1000;
-}
-
-function notify(message, duration, faIcon) {
-  var icon = '<i class="fa fa-' + faIcon + ' fa-fw"></i>';
-  var html = '<div id="notification-'
-    + notificationId + '" class="notification"><div class="notification-logo">'
-    + icon + '</div><div class="notification-text">'
-    + message + '</div></div>';
-  $('#notification-area').prepend(html);
-  var element = $('#notification-' + notificationId);
-  notificationResource[notificationId] = new Notification(element, duration);
-  notificationResource[notificationId].element.click(function() {
-    var elementId = parseInt($(this).attr('id').split('-')[1]);
-    removeNotification(elementId);
-  }).hover(function() {
-    element.fadeTo('fast', 0.5);
-  }, function() {
-    element.fadeTo('fast', 0.75);
-  });
-  element.animate({
-    opacity: 0.75,
-    right: "20px"
-  }, 500, function() {
-    element.css('right', '20px')
-  });
-  notificationId += 1;
-}
-
-function updateNotifications() {
-  for ( var key in notificationResource) {
-    var notification = notificationResource[key];
-    var time = Math.floor((new Date).getTime() - notification.initTime);
-    if (time > notification.duration) {
-      removeNotification(key);
-    }
+(function (global) {
+  'use strict';
+  
+  global.notify = global.notify || {};
+  
+  var notify = global.notify;
+  
+  var updateInterval = 500;
+  var nextId = 0;
+  
+  function remove(id) {
+    $('#notification-' + id).fadeOut('slow', function () {
+      this.remove();
+    });
   }
-}
+  
+  function update() {
+    $('.notification').each(function (index, value) {
+      var data = $(this).data('notification');
+      var time = Math.floor((new Date()).getTime() - data.time);
+      if (time > (data.duration * 1000)) {
+        remove(data.id);
+      }
+    });
+  }
+  
+  function generateHtml(id, faIcon, message) {
+    var icon = '<i class="fa fa-' + faIcon + ' fa-fw"></i>';
+    var html = '<div id="notification-'
+      + id + '" class="notification"><div class="notification-logo">'
+      + icon + '</div><div class="notification-text">'
+      + message + '</div></div>';
+    return html;
+  }
+  
+  function generateNotification(id, html, duration) {
+    $(html).prependTo('#notification-area').click(function () {
+      var data = $(this).data('notification');
+      remove(data.id);
+    }).hover(function () {
+      $(this).fadeTo('fast', 0.5);
+      console.log(typeof this);
+    }, function () {
+      $(this).fadeTo('fast', 0.75);
+      console.log(typeof this);
+    }).data('notification', {
+      'id' : id,
+      'time': (new Date()).getTime(),
+      'duration' : duration
+    }).fadeTo('fast', 0.75);
+  }
+  
+  notify.generic = function (message, duration) {
+    var html = generateHtml(nextId, 'info-circle', message);
+    generateNotification(nextId, html, duration);
+    nextId += 1;
+  };
 
-function removeNotification(id) {
-  var notification = notificationResource[id];
-  notification.element.animate({
-    opacity: 0,
-    right: "-=20px"
-  }, 500, function() {
-    notification.element.remove();
-    delete notificationResource[id];
+  notify.custom = function (message, duration, faIcon) {
+    var html = generateHtml(nextId, faIcon, message);
+    generateNotification(nextId, html, duration);
+    nextId += 1;
+  };
+  
+  $(document).ready(function () {
+    setInterval(update, updateInterval);
   });
-}
+
+}(this));
